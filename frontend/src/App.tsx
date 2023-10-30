0bf8fbd0b405fa6a6f8f928ae9e5152577d94ee6
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-10-30 16:43:02 +0100

    [FRONTEND] Footer done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MainNavbar from "./components/Layout/MainNavbar";
 import Banner from "./components/Layout/Banner/Banner";
 import BooksList from "./components/Books/BooksList/BooksList";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MainNavbar />
       <Banner />
       <BooksList />
+      <Footer />
     </>
   );
 }
