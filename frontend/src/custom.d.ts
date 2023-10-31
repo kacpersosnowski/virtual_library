49c3c85c29bdc33f3954235daea067812a8a50f4
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-10-31 22:11:13 +0100

    [FRONTEND] Improve declaring modules

--- frontend/src/custom.d.ts
+++ frontend/src/custom.d.ts
@@ -1,23 +1,7 @@
-declare module "*.svg" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
-}
+declare module "*.svg";
 
-declare module "*.jpg" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
-}
+declare module "*.jpg";
 
-declare module "*.png" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
-}
+declare module "*.png";
 
-declare module "*.css" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
-}
+declare module "*.css";
