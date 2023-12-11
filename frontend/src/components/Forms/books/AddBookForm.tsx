dfc995b07e4c64e9e7f679fce52222e2d8905a06
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-12-11 17:21:19 +0100

    VL-40 [FRONTEND] Delete comments xd

--- frontend/src/components/Forms/books/AddBookForm.tsx
+++ frontend/src/components/Forms/books/AddBookForm.tsx
@@ -19,64 +19,6 @@ import { booksApi } from "../../../config/api/books/books";
 import { snackbarActions } from "../../../store/redux/slices/snackbar-slice";
 import adminMessages from "../../../messages/adminMessages";
 
-// const adminMessages: Message = {
-//   sidebarAdminPanel: {
-//     key: "sidebar >> admin_panel",
-//     pl: "Panel admina",
-//     en: "Admin panel",
-//   },
-//   homeTab: {
-//     key: "admin_panel >> tab >> home",
-//     pl: "Strona główna",
-//     en: "Home",
-//   },
-//   addBookTab: {
-//     key: "admin_panel >> tab >> add_book",
-//     pl: "Dodaj książkę",
-//     en: "Add book",
-//   },
-//   addBookFormTitle: {
-//     key: "admin_panel >> add_book_form >> title",
-//     pl: "Tytuł",
-//     en: "Title",
-//   },
-//   addBookFormShortDescription: {
-//     key: "admin_panel >> add_book_form >> short_description",
-//     pl: "Krótki opis",
-//     en: "Short description",
-//   },
-//   addBookFormLongDescription: {
-//     key: "admin_panel >> add_book_form >> long_description",
-//     pl: "Długi opis",
-//     en: "Long description",
-//   },
-//   addBookFormAuthors: {
-//     key: "admin_panel >> add_book_form >> authors",
-//     pl: "Autorzy",
-//     en: "Authors",
-//   },
-//   addBookFormCover: {
-//     key: "admin_panel >> add_book_form >> cover",
-//     pl: "Okładka:",
-//     en: "Book cover:",
-//   },
-//   addBookFormCoverPreview: {
-//     key: "admin_panel >> add_book_form >> cover_preview",
-//     pl: "Podgląd",
-//     en: "Preview",
-//   },
-//   addBookFormCoverChooseFile: {
-//     key: "admin_panel >> add_book_form >> cover_choose_file",
-//     pl: "Wybierz plik",
-//     en: "Choose file",
-//   },
-//   addBookFormCoverSubmitButton: {
-//     key: "admin_panel >> add_book_form >> submit_button",
-//     pl: "Dodaj książkę",
-//     en: "Add book",
-//   },
-// };
-
 const AddBookForm = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
