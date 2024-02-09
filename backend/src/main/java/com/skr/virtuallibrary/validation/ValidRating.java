2ae4fdadfc418bc484d7c0342a53d19639a83712
Robert Małafiejew <01169593@pw.edu.pl>
2024-02-09 17:42:35 +0100

    quickfix

--- backend/src/main/java/com/skr/virtuallibrary/validation/ValidRating.java
+++ backend/src/main/java/com/skr/virtuallibrary/validation/ValidRating.java
@@ -6,7 +6,7 @@ import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = PasswordValidator.class)
+@Constraint(validatedBy = RatingValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidRating {
