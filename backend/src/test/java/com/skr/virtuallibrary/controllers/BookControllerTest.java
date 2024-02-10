e338b5fcd6c667752502132cda54c54c17d0649f
Kacper Sosnowski <01169627@pw.edu.pl>
2024-02-10 14:52:32 +0100

    VL-28 [BACKEND] Controller tests

---
+++ backend/src/test/java/com/skr/virtuallibrary/controllers/BookControllerTest.java
@@ -0,0 +1,89 @@
+package com.skr.virtuallibrary.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skr.virtuallibrary.auth.JwtService;
+import com.skr.virtuallibrary.dto.AuthorDto;
+import com.skr.virtuallibrary.dto.BookDto;
+import com.skr.virtuallibrary.services.BookService;
+import com.skr.virtuallibrary.services.PdfFileService;
+import org.assertj.core.api.Assertions;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.instancio.Select.field;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private PdfFileService pdfFileService;
+
+    @Test
+    void testFindBookById() throws Exception {
+        BookDto bookDto = Instancio.of(BookDto.class)
+                .set(field(BookDto::getAuthorList), Instancio.ofList(AuthorDto.class).size(2).create())
+                .create();
+
+        when(bookService.findBookById(bookDto.getId())).thenReturn(bookDto);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/books/{id}", bookDto.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        BookDto actualBookDto = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        Assertions.assertThat(actualBookDto).usingRecursiveComparison().isEqualTo(bookDto);
+
+    }
+
+    @Test
+    void testFindAllBooks() throws Exception {
+        List<BookDto> bookDtoList = Instancio.ofList(BookDto.class).size(3)
+                .set(field(BookDto::getAuthorList), Instancio.ofList(AuthorDto.class).size(2).create())
+                .create();
+
+        when(bookService.findAllBooks()).thenReturn(bookDtoList);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/books")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        List<BookDto> actualBookDtoList = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        Assertions.assertThat(actualBookDtoList).usingRecursiveComparison().isEqualTo(bookDtoList);
+    }
+
+    @Test
+    void testDeleteBook() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/books/{id}", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
+}
