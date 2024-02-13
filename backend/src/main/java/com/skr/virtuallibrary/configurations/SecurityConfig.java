62adc3dc03db599f91e02bf3de365d2564647512
Kacper Sosnowski <01169627@pw.edu.pl>
2024-02-13 19:00:47 +0100

    VL-54 [BACKEND] Security filters refactor

--- backend/src/main/java/com/skr/virtuallibrary/configurations/SecurityConfig.java
+++ backend/src/main/java/com/skr/virtuallibrary/configurations/SecurityConfig.java
@@ -24,12 +24,6 @@ public class SecurityConfig {
 
     private static final String ADMIN_AUTHORITY = "ADMIN";
 
-    private static final String BOOK_ENDPOINT = "/books/**";
-
-    private static final String AUTHOR_ENDPOINT = "/authors/**";
-
-    private static final String GENRE_ENDPOINT = "/genres/**";
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
                                            CorsConfigurationSource corsConfigurationSource) throws Exception {
@@ -39,27 +33,19 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers(HttpMethod.POST, "/auth/quick-register").hasAuthority(ADMIN_AUTHORITY)
-
-                                .requestMatchers(HttpMethod.POST, BOOK_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.PUT, BOOK_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.DELETE, BOOK_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-
-                                .requestMatchers(HttpMethod.POST, AUTHOR_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.PUT, AUTHOR_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.DELETE, AUTHOR_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-
-                                .requestMatchers(HttpMethod.POST, GENRE_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.PUT, GENRE_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-                                .requestMatchers(HttpMethod.DELETE, GENRE_ENDPOINT).hasAuthority(ADMIN_AUTHORITY)
-
-                                .requestMatchers(HttpMethod.GET, BOOK_ENDPOINT).permitAll()
-                                .requestMatchers(HttpMethod.GET, "/files/cover/**").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll()
-                                .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 .requestMatchers("/error").anonymous()
-                                .anyRequest().authenticated()
+
+                                .requestMatchers(HttpMethod.GET, "/books/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/authors/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/files/cover/**").permitAll()
+                                .requestMatchers("/reviews/**").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/email/**").permitAll()
+
+                                .requestMatchers(HttpMethod.POST, "/auth/quick-register").hasAuthority(ADMIN_AUTHORITY)
+                                .anyRequest().hasAuthority(ADMIN_AUTHORITY)
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
