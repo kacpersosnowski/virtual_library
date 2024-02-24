561dbec8134b04d07a8ea19897220171441de9d7
Robert Małafiejew <01169593@pw.edu.pl>
2024-02-24 17:12:51 +0100

    restricted adding reviews

--- backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
+++ backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
@@ -7,6 +7,7 @@ import com.skr.virtuallibrary.entities.Review;
 import com.skr.virtuallibrary.entities.User;
 import com.skr.virtuallibrary.entities.enums.Authority;
 import com.skr.virtuallibrary.exceptions.BookNotFoundException;
+import com.skr.virtuallibrary.exceptions.ReviewAlreadyExistsException;
 import com.skr.virtuallibrary.exceptions.ReviewNotFoundException;
 import com.skr.virtuallibrary.exceptions.UserNotFoundException;
 import com.skr.virtuallibrary.mapping.ModelMapper;
@@ -85,6 +86,13 @@ public class ReviewService {
         Review review = modelMapper.toReviewEntity(reviewDto);
         Book book = bookRepository.findById(reviewDto.getBookId())
                 .orElseThrow(() -> new BookNotFoundException(BOOK_NOT_FOUND_MSG + reviewDto.getBookId()));
+
+        boolean notHaveReviewOrAdmin = reviewRepository
+                .findAllByBookIdAndAuthorId(reviewDto.getBookId(), getUser().getId()).isEmpty()
+                || getUser().getAuthority().equals(Authority.ADMIN);
+        if (notHaveReviewOrAdmin) {
+            throw new ReviewAlreadyExistsException("You have already reviewed this book.");
+        }
 
         review.setBook(book);
         review.setAuthor(getUser());
