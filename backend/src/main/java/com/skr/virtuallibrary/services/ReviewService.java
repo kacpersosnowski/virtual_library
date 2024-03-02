8f1a2cee70c910ad7e67b1f43a23239ccc592949
Robert Małafiejew <01169593@pw.edu.pl>
2024-03-02 17:17:03 +0100

    VL-69 [BACKEND] merge problems resolved

--- backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
+++ backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
@@ -2,7 +2,6 @@ package com.skr.virtuallibrary.services;
 
 import com.skr.virtuallibrary.controllers.responses.PagedResponse;
 import com.skr.virtuallibrary.dto.ReviewDto;
-import com.skr.virtuallibrary.entities.Book;
 import com.skr.virtuallibrary.entities.Review;
 import com.skr.virtuallibrary.entities.User;
 import com.skr.virtuallibrary.entities.enums.Authority;
@@ -98,7 +97,6 @@ public class ReviewService {
             throw new ReviewAlreadyExistsException("You have already reviewed this book.");
         }
 
-        review.setBook(book);
         review.setAuthor(getUser());
 
         reviewDto = modelMapper.toReviewDto(reviewRepository.save(review));
