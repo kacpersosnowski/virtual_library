e4f2d7496fb37a6fdb0b9c1cbb7a5b8f133c617c
Robert Małafiejew <01169593@pw.edu.pl>
2024-03-03 15:51:28 +0100

    VL-69 [BACKEND] duplicated methods deleted

--- backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
+++ backend/src/main/java/com/skr/virtuallibrary/services/ReviewService.java
@@ -1,6 +1,5 @@
 package com.skr.virtuallibrary.services;
 
-import com.skr.virtuallibrary.controllers.responses.PagedResponse;
 import com.skr.virtuallibrary.dto.ReviewDto;
 import com.skr.virtuallibrary.entities.Review;
 import com.skr.virtuallibrary.entities.User;
@@ -14,17 +13,12 @@ import com.skr.virtuallibrary.repositories.BookRepository;
 import com.skr.virtuallibrary.repositories.ReviewRepository;
 import com.skr.virtuallibrary.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,46 +36,6 @@ public class ReviewService {
     private final BookRepository bookRepository;
 
     private static final String ERROR_NOT_FOUND_MSG = "Not found review with id: ";
-
-    public static final String BOOK_NOT_FOUND_MSG = "Not found book with id: ";
-
-    public ReviewDto findReviewById(String id) {
-        return reviewRepository.findById(id).map(modelMapper::toReviewDto)
-                .orElseThrow(() -> new ReviewNotFoundException(ERROR_NOT_FOUND_MSG + id));
-    }
-
-    public List<ReviewDto> findAllReviews() {
-        return reviewRepository.findAll()
-                .stream()
-                .map(modelMapper::toReviewDto)
-                .toList();
-    }
-
-    public PagedResponse<ReviewDto> findReviewsByBookId(String id, Integer pageNr) {
-        if (pageNr < 0) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Page number cannot be negative.");
-        }
-
-        if (bookRepository.findById(id).isEmpty()) {
-            throw new BookNotFoundException(BOOK_NOT_FOUND_MSG + id);
-        }
-
-        Pageable pageable = PageRequest.of(pageNr, 10, Sort.by("auditData.lastModifiedDate").descending());
-        Page<Review> reviews = reviewRepository.findAllByBookId(id, pageable);
-
-        return new PagedResponse<>(
-                reviews.getTotalElements(),
-                reviews.stream().map(modelMapper::toReviewDto).toList()
-        );
-    }
-
-    public PagedResponse<ReviewDto> findReviewsByBookId(String id) {
-        if (bookRepository.findById(id).isEmpty()) {
-            throw new BookNotFoundException(BOOK_NOT_FOUND_MSG + id);
-        }
-        List<ReviewDto> reviews = reviewRepository.findAllByBookId(id).stream().map(modelMapper::toReviewDto).toList();
-        return new PagedResponse<>(reviews);
-    }
 
     public ReviewDto addReview(ReviewDto reviewDto) {
         if (bookRepository.findById(reviewDto.getBookId()).isEmpty()) {
