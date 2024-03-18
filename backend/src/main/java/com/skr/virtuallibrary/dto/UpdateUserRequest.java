499120498257a70403806817cececa0314cdab50
Kacper Sosnowski <01169627@pw.edu.pl>
2024-03-18 21:04:47 +0100

    VL-83 [BACKEND] Added update user endpoint

--- backend/src/main/java/com/skr/virtuallibrary/dto/UpdateUserRequest.java
+++ backend/src/main/java/com/skr/virtuallibrary/dto/UpdateUserRequest.java
@@ -1,24 +1,12 @@
 package com.skr.virtuallibrary.dto;
 
-import com.skr.virtuallibrary.entities.enums.Authority;
 import com.skr.virtuallibrary.entities.enums.Language;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserDto {
-
-    private String id;
-
-    @NotEmpty
-    private String username;
+public class UpdateUserRequest {
 
     @NotEmpty
     private String firstName;
@@ -28,9 +16,6 @@ public class UserDto {
 
     @NotEmpty
     private String email;
-
-    @NotNull
-    private Authority authority;
 
     @NotNull
     private Language language;
