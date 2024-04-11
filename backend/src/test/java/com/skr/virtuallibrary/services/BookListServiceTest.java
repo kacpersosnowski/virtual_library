0dc55a809cf5ae8604748183fc778dd11fe03571
Robert Małafiejew <rmalafiej@gmail.com>
2024-04-11 08:21:59 +0200

    VL-86 [BACKEND] service tests added

--- backend/src/test/java/com/skr/virtuallibrary/services/BookListServiceTest.java
+++ backend/src/test/java/com/skr/virtuallibrary/services/BookListServiceTest.java
@@ -4,6 +4,7 @@ import com.skr.virtuallibrary.dto.BookListDto;
 import com.skr.virtuallibrary.entities.BookList;
 import com.skr.virtuallibrary.entities.User;
 import com.skr.virtuallibrary.entities.enums.Authority;
+import com.skr.virtuallibrary.entities.enums.Language;
 import com.skr.virtuallibrary.mapping.ModelMapper;
 import com.skr.virtuallibrary.repositories.BookListRepository;
 import org.instancio.Instancio;
@@ -14,10 +15,11 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.instancio.Select.field;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = {BookListRepository.class})
@@ -44,8 +46,10 @@ class BookListServiceTest {
     @BeforeEach
     final void setUp() {
         exampleUser = User.builder()
+                .id("userFoo")
                 .email("example@example.com")
                 .authority(Authority.USER)
+                .language(Language.ENG)
                 .username(USERNAME)
                 .password(PASSWORD)
                 .build();
@@ -58,9 +62,11 @@ class BookListServiceTest {
         String idToFind = "foo";
         BookListDto bookListToFindDto = Instancio.of(BookListDto.class)
                 .set(field(BookListDto::getBookIds), Instancio.ofList(String.class).size(2).create())
+                .set(field(BookListDto::getUserId), exampleUser.getId())
                 .create();
         BookList bookListToFind = Instancio.of(BookList.class)
-                .set(field(BookListDto::getBookIds), Instancio.ofList(String.class).size(2).create())
+                .set(field(BookList::getBookIds), Instancio.ofList(String.class).size(2).create())
+                .set(field(BookList::getUserId), exampleUser.getId())
                 .create();
 
         // when
@@ -72,6 +78,35 @@ class BookListServiceTest {
         BookListDto actual = bookListService.getBookList(idToFind);
 
         // then
-        assertEquals(expected, actual);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD)
+    void findBookLists_shouldReturnBookListDto() {
+        // given
+        List<BookList> bookLists = Instancio.ofList(BookList.class).size(3)
+                .set(field(BookList::getBookIds), Instancio.ofList(String.class).size(3).create())
+                .set(field(BookList::getUserId), exampleUser.getId())
+                .create();
+        List<BookListDto> bookListDtos = Instancio.ofList(BookListDto.class).size(3)
+                .set(field(BookListDto::getBookIds), Instancio.ofList(String.class).size(3).create())
+                .set(field(BookListDto::getUserId), exampleUser.getId())
+                .create();
+
+        // when
+        when(userService.getCurrentUser()).thenReturn(exampleUser);
+        when(bookListRepository.findAllByNameAndUserId("To Read", exampleUser.getId()).isEmpty())
+                .thenReturn(false);
+        when(bookListRepository.findAllByUserId(exampleUser.getId())).thenReturn(bookLists);
+        for (int i = 0; i < bookLists.size(); i++) {
+            when(modelMapper.toBookListDto(bookLists.get(i)))
+                    .thenReturn(bookListDtos.get(i));
+        }
+        List<BookListDto> expected = bookListDtos;
+        List<BookListDto> actual = bookListService.getBookLists();
+
+        // then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 }
